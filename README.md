88d7585fcb19cb665652eb65e186eab9b134a191
Igor Avelino <igoravel21@gmail.com>
2023-01-25 23:18:34 -0300

    termina projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+# React/Ts Multiselect Dropdown
+
+## O que é?
+
+Dois simples componentes `select` feitos a mão, sendo um deles com a capacidade de adicionar mais de um item dentro, desenvolvidos com acessibilidade em mente, podendo se utilizar do teclado para navegar e adicionar itens no componente.
+
+## Tecnologia usada
+
+React e Typescript.
---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Select, SelectOption } from "./components/Select";
+import styles from "./Select.module.css";
+const options = [
+  {
+    content: "Primeiro",
+    value: 1,
+  },
+  {
+    content: "Segundo",
+    value: 2,
+  },
+  {
+    content: "Terceiro",
+    value: 3,
+  },
+  {
+    content: "Quarto",
+    value: 4,
+  },
+  {
+    content: "Quinto",
+    value: 5,
+  },
+];
+
+function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
+  return (
+    <>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(opt) => setValue1(opt)}
+      />
+      <br />
+      <Select
+        options={options}
+        value={value2}
+        onChange={(opt) => setValue2(opt)}
+      />
+    </>
+  );
+}
+
+export default App;
